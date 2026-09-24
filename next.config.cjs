4f60b0adda5b738f4e4cdd5e23d3c941a4bce0ad
praveennnnnnjr <praveenkumar29072006@gmail.com>
2026-09-24 12:50:21 +0530

    Fix syntax error in postcss.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* ungaloda config options edhadhu irundha inga irukkum */
-};
+const nextConfig = {};
 
-export default nextConfig;
+module.exports = nextConfig;
